e9d1092390060d13bc67df9ca8b49b6bf287186c
kgulzina <gkuttubekova@gmail.com>
2019-04-22 13:52:16 -0500

    Updated test files for both functions

--- tests/testthat/test-bootstrapprocess.R
+++ tests/testthat/test-bootstrapprocess.R
@@ -1,5 +1,19 @@
 context("test-bootstrapprocess")
 
-# test_that("multiplication works", {
-#   expect_equal(2 * 2, 4)
-# })
+test_that("sample dataset is valid", {
+  expect_error(bootstrapProcess(c(), T))
+  expect_error(bootstrapProcess(diag(1, nrow = 2), T))
+  expect_error(bootstrapProcess(c(8, 13, NA), T))
+  expect_error(bootstrapProcess(rnorm, T))
+})
+
+test_that("anime value is valid", {
+  expect_error(bootstrapProcess(1:30), anime = NA)
+  expect_error(bootstrapProcess(1:30), anime = NULL)
+  expect_error(bootstrapProcess(1:30), anime = Inf)
+  expect_error(bootstrapProcess(1:30), anime = NaN)
+  expect_error(bootstrapProcess(1:30), anime = 8)
+  expect_error(bootstrapProcess(1:30), anime =  1.3)
+  expect_error(bootstrapProcess(1:30), anime = "cats")
+  expect_error(bootstrapProcess(1:30), anime = t)
+})
--- tests/testthat/test-bootstrapsample.R
+++ tests/testthat/test-bootstrapsample.R
@@ -1,8 +1,19 @@
 context("test-bootstrapsample")
 
-test_that("sample dataset is valid", {
+test_that("n is valid", {
   expect_error(bootstrapSample(1:30, 'a'))
-  expect_error(bootstrapSample(1:30, 1.3))
+  expect_error(bootstrapSample(1:30, NA))
   expect_error(bootstrapSample(1:30, 1:3))
-  expect_error(bootstrapSample(1:30, 'a'))
+  expect_error(bootstrapSample(1:30, NaN))
+  expect_error(bootstrapSample(1:30, Inf))
+  expect_error(bootstrapSample(1:30, pi))
+  expect_error(bootstrapSample(1:30, NULL))
 })
+
+test_that("sample dataset is valid", {
+  expect_error(bootstrapSample(c(), 8))
+  expect_error(bootstrapSample(diag(1, nrow = 2), 8))
+  expect_error(bootstrapSample(c(8, 13, NA), 8))
+  expect_error(bootstrapSample(rnorm, 8))
+})
+
