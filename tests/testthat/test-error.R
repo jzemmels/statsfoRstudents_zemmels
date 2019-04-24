c83e260a52c85be6163384feb9e77c442ceb14c2
Eryn B <eblagg@iastate.edu>
2019-04-24 17:17:12 -0500

    errors on errors

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -14,11 +14,4 @@ test_that("arguments are checked", {
   expect_error(ploterrors(plotly=NA))
 })
 
-test_that("output is checked",{
-  expect_class(ploterrors(),c("gg","ggplot"))
-  expect_class(ploterrors(direction = ">"),c("gg","ggplot"))
-  expect_class(ploterrors(direction = "<"),c("gg","ggplot"))
-  expect_class(ploterrors(plotly=TRUE),c("plotly","htmlwidget"))
-  expect_class(ploterrors(plotly=TRUE,direction = ">"),c("plotly","htmlwidget"))
-  expect_class(ploterrors(plotly=TRUE,direction = "<"),c("plotly","htmlwidget"))
-})
+
