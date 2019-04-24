f863314d0a9b0bc7b36bbc09246cb692b866f21f
c-roiger <charlotte.roiger@gmail.com>
2019-04-24 16:52:01 -0500

    update documentation

--- inst/shiny-examples/charlotte/server.R
+++ inst/shiny-examples/charlotte/server.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 server <- function(input, output) {
-  
+
   output$aspect <- renderUI({
     if(input$description=="Modal") {
       selectInput(selected = "choose",
@@ -10,7 +10,7 @@ server <- function(input, output) {
       )
     }
   })
-  
+
   observeEvent(input$redo, {
     randomDist <- sample(1:3,1)
       randomModality <- case_when(
@@ -18,19 +18,19 @@ server <- function(input, output) {
         randomDist ==2 ~ c("rbeta(10000,2,5)", 'unimod', '.1'),
         randomDist ==3 ~ c("c(rbeta(5000,10,2), rbeta(5000,1,5))", 'bimod', '.1')
       )
-      
+
       randomShape <- case_when(
         randomDist == 1 ~ c("rbeta(10000,2,5)", 'rs'),
         randomDist ==2 ~ c("rbeta(10000,5,2)", 'ls'),
         randomDist ==3 ~ c("rbeta(10000,5,5)", 'sym')
       )
-    
+
     randomOutlier <- case_when(
       randomDist == 1 ~ c('runif(1,0,1)', "unif"),
       randomDist ==2 ~ c("rbeta(10000,2,5)", 'unimod'),
       randomDist ==3 ~ c("rbeta(10000,2,5) + rbeta(10000,5,2)", 'bimod')
     )
-    
+
     output$plot <- renderPlot({
       dist <- eval(parse(text=randomModality[1]))
       dist %>%
@@ -38,12 +38,12 @@ server <- function(input, output) {
         ggplot(aes(x = dist)) +
         geom_histogram(binwidth = as.numeric(randomModality[3]))
     })
-    
+
     output$feedback <- renderText({
       print(input$answer)
       print(randomModality)
-      
-      
+
+
       if(input$description=="Modal")
       {
         if(input$answer==randomModality[2]) {
@@ -52,14 +52,14 @@ server <- function(input, output) {
         }
         else {"Not Correct, Select Another"}
       }
-    
-      
-      
+
+
+
     })
-    
+
   })
-  
 
 
-  
+
+
 }
