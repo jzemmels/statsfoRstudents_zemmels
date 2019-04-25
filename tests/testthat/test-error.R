26c74f0b1e79ed327d5e16f07a2be4b849e430f0
Eryn B <eblagg@iastate.edu>
2019-04-24 20:03:53 -0500

    committing changes to test_errors

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -1,5 +1,10 @@
 context("test-error")
 
+
+set.seed(4859374)
+x <- randomSample()
+m <- updateSampleMeans(sampleData = x)
+
 test_that("arguments are checked", {
   expect_error(ploterrors(means="a"))
   expect_error(ploterrors(sds="a"))
@@ -13,6 +18,17 @@ test_that("arguments are checked", {
   expect_error(ploterrors(direction = NA))
   expect_error(ploterrors(plotly=NA))
 })
+
+test_that("plotup output is checked",{
+  expect_class(ploterrors$pltup(sampleData = x),c("gg","ggplot"))
+  expect_class(ploterrors$pltup(sampleData = x,plotly=TRUE),c("plotly","htmlwidget"))
+})
+
+test_that("plotlw output is checked",{
+  expect_class(ploterrors$pltlw(sampleData = x),c("gg","ggplot"))
+  expect_class(ploterrors$pltlw(sampleData = x,plotly=TRUE),c("plotly","htmlwidget"))
+})
+
 
 test_that("output is checked",{
   expect_class(ploterrors(),"list")})
